23a25e7b664caa81382785040e813fe50b6f1a8c
lundbmp <m.lundberg008@gmail.com>
2022-05-14 15:01:15 -0500

    inital planning and comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
 // Assignment code here
+/*
+generatePassword() will be a function with a return type of string.
+  will contain forloop, index will be the length of the desired password
+  randomNumber(x,y) will return a number to be added to a string containing unicode prefix
+    string will be converted from unicode to the desired character and appended to a string
+    string will be returned
+
+need a randomNumber function with min and max perameters.
+*/
 
 
 // Get references to the #generate element
