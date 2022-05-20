9db54b0d268ab35fea06e1d94f2776cc1536e63d
lundbmp <m.lundberg008@gmail.com>
2022-05-19 22:59:57 -0500

    generate password works --- clean up code tomorrow

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,14 @@ need a randomNumber function with min and max perameters.
 */
 
 function generatePassword() {
+  let charArray = [];
+  let userPassword = "";
+
+  let lowerCaseCharArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  let upperCaseCharArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  let numberArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  let specialCharArr = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_"]; 
+
   let userInputs = {
     promptInputCharLength : function() {
       let charLenghtInput = prompt("What length password would you like? (8-128 characters long)");
@@ -30,8 +38,24 @@ function generatePassword() {
       userInputs.specialCharacters = confirm("would you like special characters in your password? ");
     }
   }
-
+  userInputs.promptInputCharLength();
   
+  if(userInputs.lowerCase) { charArray = charArray.concat(lowerCaseCharArr); }
+  if(userInputs.upperCase) { charArray = charArray.concat(upperCaseCharArr); }
+  if(userInputs.numbers) { charArray = charArray.concat(numberArr); }
+  if(userInputs.specialCharacters) { charArray = charArray.concat(specialCharArr); }
+
+  // error case do not delete
+  if(charArray.length < 1) { 
+    alert("You need to select what characters you want. Try again.");
+    throw new Error('You need to select what characters you want. Try again.');
+  }
+
+  for(let index = 0; index < userInputs.characterLength; index++) {
+    userPassword += charArray[randomNumber(0, charArray.length - 1)];
+  }
+  
+  return userPassword;
 }
 
 function randomNumber(min, max) {
